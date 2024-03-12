04784b866098dc57740a0ee2aca69ae6b02d96c9
nurhaziq97 <muhammadnurhaziq97@gmail.com>
2024-03-12 18:23:32 +0800

    update utils and payloads

---
+++ src/main/java/com/demo/common/utils/Constants.java
@@ -0,0 +1,10 @@
+package com.demo.common.utils;
+
+public class Constants {
+
+    private Constants(){
+        throw new IllegalStateException(UTILITY_CLASS);
+    }
+    public static final String UTILITY_CLASS = "Utility Class";
+    public static final String DATE_TIME_FORMAT="yyyy-MMM-dd[ HH:mm:ss.SSS]";
+}
